2bc41562cf9ce4baffc39b3e40a331553559d1e9
Robert Thompson <rbtprograms@gmail.com>
2019-04-12 23:54:30 -0700

    header looks good, nextjs creates so much extra code tho my god

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "728c390caace0e4ee6f2";
+/******/ 	var hotCurrentHash = "b295082c933936818dab";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
