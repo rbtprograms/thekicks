53843b91c63fb64c95643cab25c2061cf1c2884a
Robert Thompson <rbtprograms@gmail.com>
2019-04-15 21:11:15 -0700

    got rid of the flicker using a styled components server side rendering specific funciton;

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c15cb7f0675c544798dd";
+/******/ 	var hotCurrentHash = "9861eced730e33fc1b4f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
