7494c39ba1afe35404ed23d58c6be7ab28477147
Robert Thompson <rbtprograms@gmail.com>
2019-05-24 22:57:42 -0700

    i guess its working does not feel super typescripty tho

--- backend/src/resolvers/Mutation.ts
+++ backend/src/resolvers/Mutation.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  async createItem(parent, args, context, info) {
+  async createItem(args, context, info) {
     const item = await context.db.mutation.createItem({
       data: {
         ...args
