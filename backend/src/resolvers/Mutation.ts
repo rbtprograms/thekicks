fcacad32fd4b4d7ea7f4a9d5133e7e1d25689324
Robert Thompson <rbtprograms@gmail.com>
2019-05-30 07:19:21 -0700

    finally got update item working!

--- backend/src/resolvers/Mutation.ts
+++ backend/src/resolvers/Mutation.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  async createItem(args, context, info) {
+  async createItem(parent, args, context, info) {
     const item = await context.db.mutation.createItem({
       data: {
         ...args
@@ -7,7 +7,7 @@ module.exports = {
     }, info);
     return item;
   },
-  updateItem(/*parent, */args, context, info) {
+  updateItem(parent, args, context, info) {
     const updates = { ...args };
     delete updates.id;
     return context.db.mutation.updateItem({
