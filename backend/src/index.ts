4923577ea0acbc1f2ed072acbd6a7ec0c3e3d3bd
Robert Thompson <rbtprograms@gmail.com>
2019-07-30 07:12:10 -0700

    got the response through(literally just had to compile) but now the cookie is not set so now i have to do that

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,25 @@
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
-require('dotenv').config({ path: 'variables.env' });
+require("dotenv").config({ path: "variables.env" });
 
-const server = require('./createServer')();
+const server = require("./createServer")();
 
 //express middleware for JWT and populate user
 server.express.use(cookieParser());
-
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL
-  },
-}, ({ port }: { port: number }) => {
-  console.log(`server is now running on port ${port}`);
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  console.log('COOKIES', req.cookies);
+  next();
 });
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  ({ port }: { port: number }) => {
+    console.log(`server is now running on port ${port}`);
+  }
+);
