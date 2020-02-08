9b84186ce66d5ac6c26c33aaaa07a52f906c34ca
Robert Thompson <rbtprograms@gmail.com>
2020-02-07 23:58:10 -0800

    fixed compiler error

--- frontend/components/Permissions.tsx
+++ frontend/components/Permissions.tsx
@@ -1,31 +1,27 @@
-// import { Query } from 'react-apollo';
-// import gql from 'graphql-tag';
-// import Error from './DisplayError';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import Error from './DisplayError';
 
-// const ALL_USERS_QUERY = gql`
-//   query {
-//     users {
-//       id
-//       name
-//       email
-//       permissions
-//     }
-//   }
-// `
+const ALL_USERS_QUERY = gql`
+  query {
+    users {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`;
 
 const Permissions = () => (
-  // <Query
-  //   query={ALL_USERS_QUERY}
-  // >
-  //   {({ data, loading, error}) => (
-  //     <div>
-  //       <Error error={error} />
-  //     </div>
-  //   )}
-  // </Query>
-  <div>
-    Hi
-  </div>
-)
+  <Query query={ALL_USERS_QUERY}>
+    {({ error }) => (
+      <div>
+        <Error message={error} />
+        <p>Hey</p>
+      </div>
+    )}
+  </Query>
+);
 
 export default Permissions;
